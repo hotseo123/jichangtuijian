357180d4849deeeee695dfcb587481c7c6816c55
hot123 <jackiszing@foxmail.com>
2026-01-13 15:55:57 +0800

    Delete data/getSites.server.ts

--- data/getSites.server.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export function getSites() {
-  const file = path.join(process.cwd(), 'data', 'sites.json');
-  return JSON.parse(fs.readFileSync(file, 'utf8')).sites;
-}
