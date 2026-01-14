3e2aa55a9b7811e25610e88725d438eb74aa2511
jackiszing <jackiszing@gmail.com>
2026-01-14 19:50:20 +0800

    support npm run export

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isExport = process.env.BUILD_MODE === 'export'
+
 const nextConfig = {
+  output: isExport ? 'export' : undefined,
+  trailingSlash: isExport, // export 时自动加 /
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
- 
 }
 
 export default nextConfig
